ef971761ca6a7d075482c7e07924f1b8371d21cc
Imre Kószó <imrekoszo@users.noreply.github.com>
2022-06-16 14:00:46 +0200

    WIP: break test setup

--- projects/example/test/polylith_kaocha/example/test_setup.clj
+++ projects/example/test/polylith_kaocha/example/test_setup.clj
@@ -1,7 +1,8 @@
 (ns polylith-kaocha.example.test-setup)
 
 (defn setup [project-name]
-  (println (str "--- test setup for " project-name " ---")))
+  (println (str "--- test setup for " project-name " ---"))
+  (throw (Exception. "SETUP FAILED")))
 
 (defn teardown [project-name]
   (println (str "--- test teardown for " project-name " ---")))
